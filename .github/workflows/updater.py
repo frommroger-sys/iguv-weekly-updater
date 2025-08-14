5d9ef2c6fb12dfca513c157b717c18b6be1bccca
frommroger-sys <fromm.roger@gmail.com>
2025-08-14 15:42:28 +0200

    Create updater.py

---
+++ .github/workflows/updater.py
@@ -0,0 +1,11 @@
+- def fetch_so-fit():
++ def fetch_so_fit():
+     return fetch_ao_generic("https://www.so-fit.ch",
+         ["gebühr","gebuehr","tarif","reglement","prüf","pruef","faq"])
+
+-     aos_full = { "AOOS": fetch_aoos(), "OSFIN": fetch_osfin(),
+-                  "OAD FCT":fetch_oadfct(), "OSIF": fetch_osif(),
+-                  "So-Fit": fetch_so-fit() }
++     aos_full = { "AOOS": fetch_aoos(), "OSFIN": fetch_osfin(),
++                  "OAD FCT": fetch_oadfct(), "OSIF": fetch_osif(),
++                  "So-Fit": fetch_so_fit() }
