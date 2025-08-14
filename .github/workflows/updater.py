784927acdcbf77deded6d16c65cf13d6703c73bb
frommroger-sys <fromm.roger@gmail.com>
2025-08-14 15:44:18 +0200

    Update updater.py

--- .github/workflows/updater.py
+++ .github/workflows/updater.py
@@ -5,7 +5,7 @@
 
 -     aos_full = { "AOOS": fetch_aoos(), "OSFIN": fetch_osfin(),
 -                  "OAD FCT":fetch_oadfct(), "OSIF": fetch_osif(),
--                  "So-Fit": fetch_so-fit() }
+-                  "So-Fit": fetch_so_fit() }
 +     aos_full = { "AOOS": fetch_aoos(), "OSFIN": fetch_osfin(),
 +                  "OAD FCT": fetch_oadfct(), "OSIF": fetch_osif(),
 +                  "So-Fit": fetch_so_fit() }
